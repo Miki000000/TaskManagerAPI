5d57d539e049444a6f0850ddb9e049b2f6ab0830
Vitor <vitorhugosnp@gmail.com>
2025-03-05 23:50:54 -0400

    fix: Fixed password encoder configuration

--- src/main/kotlin/com/api/task_manager/configurations/DatabaseInitializer.kt
+++ src/main/kotlin/com/api/task_manager/configurations/DatabaseInitializer.kt
@@ -4,20 +4,20 @@ import com.api.task_manager.features.user.User
 import com.api.task_manager.features.user.UserRepository
 import com.api.task_manager.features.user.UserRoles
 import org.springframework.boot.CommandLineRunner
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseInitializer(
     private val userRepository: UserRepository,
-    private val passwordEncoder: BCryptPasswordEncoder
+    private val passwordEncoder: PasswordEncoder
 ) : CommandLineRunner {
     override fun run(vararg args: String) {
         if (!userRepository.existsByEmail("admin@admin.com")) {
             val adminUser = User(
                 email = "admin@admin.com",
                 username = "admin",
-                password = passwordEncoder.encode("1234"),
+                password = passwordEncoder.encode("admin1234"),
                 role = UserRoles.ADMIN
             )
             userRepository.save(adminUser)
